625d740e43ca6ff1d9ea24962613656b1211c5c6
khawarizmi02 <khawarizmijefri02@gmail.com>
2023-03-10 18:24:33 +0800

    upload codes

---
+++ src/components/Tech.jsx
@@ -0,0 +1,31 @@
+// import React from 'react'
+//
+// const Tech = () => {
+//   return (
+//     <div>Tech</div>
+//   )
+// }
+//
+// export default Tech
+
+
+// Raw code lines
+import React from "react";
+
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+
+const Tech = () => {
+  return (
+      <div className='flex flex-row flex-wrap justify-center gap-10'>
+        {technologies.map((technology) => (
+            <div className='w-28 h-28' key={technology.name}>
+              <BallCanvas icon={technology.icon} />
+            </div>
+        ))}
+      </div>
+  );
+};
+
+export default SectionWrapper(Tech, "");
